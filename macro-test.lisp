7ff181df99ef839663c8cdc4528def82759fdc37
Joseph Oswald <jao@joseph-oswalds-ibook-g4.local>
2008-07-15 22:19:48 -0400

    Progress on sketching expansion
    Made some progress in sketching out a simple example of how a simple
    COMFY macro expander could be defined.

--- macro-test.lisp
+++ macro-test.lisp
@@ -28,4 +28,46 @@
 					      (append car-bindings
 						      existing)))))))
     
-  
+
+(defun rr-expander (form)
+  "Returns two values, the first is T if a match was achieved,
+the second is then the expansion.
+   If no match was possible, the first value is NIL and the 
+second is the original form"
+  (multiple-value-bind (match-1 expansion-1)
+      (let ((car-var (car form))
+	    (cdr-var (cdr form)))
+	(if (eql car-var 'rr-example)
+	    (if (null cdr-var)
+		(values t '(8 rl))
+		(values nil form))
+	    (values nil form)))
+    (if match-1 (values match-1 expansion-1)
+	(multiple-value-bind (match-2 expansion-2)
+	    (let ((car-var (car form))
+		  (cdr-var (cdr form)))
+	      (if (eql car-var 'rr-example)
+		  (let ((?place (car cdr-var))
+			(cddr-var (cdr cdr-var)))
+		    (if (null cddr-var)
+			(values t `(seq push (l ,@?place) 
+					rr-example (st ,@?place) 
+					pop))
+			(values nil form)))
+		  (values nil form)))
+	  (if match-2 
+	      (values match-2 expansion-2)
+	      (values nil form))))))
+
+(rr-expander '(rr-example)) 
+
+;; --> T, (8 RL)
+
+(rr-expander '(rr-example (loc)))
+
+;; --> T, (SEQ PUSH (L LOC) RR-EXAMPLE (ST LOC) POP)
+
+(rr-expander '(rr-mismatch))
+
+;; --> NIL, (RR-MISMATCH)
+                   	  
